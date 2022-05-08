11e6c56c5e0dded5f1c05ef3881a0bb8f5bbd3b6
Ricky Felix <rickyfelix19@outlook.com>
2022-05-08 11:48:35 +1000

    create tailwind components

---
+++ frontend/src/components/button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+  return (
+    <div>
+      <div>
+        <h1 className="text-title">Hello World</h1>
+      </div>
+      <div className="button-primary">Button</div>
+    </div>
+  );
+}
+
+export default Button;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    fontSize: {
+      title: ['96px'],
+      H1: ['72px'],
+      H2: ['42px'],
+      H3: ['24px'],
+      H4: ['20px'],
+      paragraph: ['18px'],
+      subtitle: ['16px']
+    }
   },
   plugins: []
 };
